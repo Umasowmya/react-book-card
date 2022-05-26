687d254a5e06b4391afc3bd3856045c050ff1a49
Umasowmya <uma.sowmya@zemosolabs.com>
2022-05-26 08:58:10 +0530

    changes committed

---
+++ src/MyComponents/About.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      This is about component
+      <p>
+      On Dec 29, 2020, I had a little reunion with my college friends and my former professor
+       in a zoom meeting. My former professor is Dicky Arinal, and he is working for Disney+ now. 
+       The reunion reminded me of when Pak Dicky(We called him using Pak in Indonesia) showed off 
+       one of his magics using Emmet, and it made me amazed. However, at that time we were still using
+        Visual Studio because he taught ASP.NET, and we needed to install Web Essentials for using Zen Coding, 
+        which is the former name of Emmet.
+
+
+      </p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/MyComponents/AddTodo.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+
+
+const AddTodo = (props) => {
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
+
+    const submit = (e) => {
+        e.preventDefault();
+        if (!title || !desc)
+            alert("Title or Description cannot be blank");
+        else {
+            props.addTodo(title, desc);
+
+            setTitle("");
+            setDesc("");
+        }
+
+    }
+
+    return (
+
+        <div className='container'>
+            <h3>Add a todo</h3>
+            <form onSubmit={submit}>
+                <div className="mb-3">
+                    <label htmlFor="title" className="form-label">Todo title</label>
+                    <input type="text" className="form-control" value={title} onChange={(e) => { setTitle(e.target.value) }} id="title" aria-describedby="emailHelp" />
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="desc" className="form-label" >Todo Description</label>
+                    <input type="text" className="form-control" id="desc" value={desc} onChange={(e) => { setDesc(e.target.value) }} />
+                </div>
+
+                <button type="submit" className="btn btn-primary">Add Todo</button>
+            </form>
+        </div>
+    )
+}
+
+export default AddTodo
